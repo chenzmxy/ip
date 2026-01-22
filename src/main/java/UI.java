638af59d4d82f82495b197b1fd01ef060ead77b8
chen_haozhe <chenzmxy@gmail.com>
2026-01-22 18:33:42 +0800

    src/main/java/ComSci.java

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class UI {
+    private static final String LINE =
+            "————————————————————————————————";
+    public void greeting() {
+        System.out.println(LINE + "\n"
+                        + "Hello! I'm ComSci\n"
+                        + "What can I do for you?\n"
+                        + LINE
+        );
+    }
+
+    public void echo(String s) {
+        System.out.println(
+                LINE + "\n"
+                + s + "\n"
+                + LINE
+        );
+    }
+
+    public void showBye() {
+        System.out.println(LINE + "\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + LINE
+        );
+    }
 }
