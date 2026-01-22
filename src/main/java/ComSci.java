5c55f2b4fb8cbdda9d3c999031aae4662ead1bac
chen_haozhe <chenzmxy@gmail.com>
2026-01-22 18:25:45 +0800

    src/main/java/ComSci.java

---
+++ src/main/java/ComSci.java
@@ -0,0 +1,14 @@
+
+
+public class ComSci {
+    private static final String LINE = "————————————————————————";
+    public static void main(String[] args) {
+        System.out.println(LINE + "\n"
+                + "Hello! I'm ComSci\n"
+                + "What can I do for you?\n"
+                + LINE + "\n"
+                + "Bye. Hope to see you again soon!\n"
+                + LINE);
+    }
+}
+
